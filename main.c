cdd8bb42766718cb527dfe6eb5ef5dfa24092a40
Victor Dantas <victorr.dantas@gmail.com>
2023-12-27 16:58:51 -0400

    Revert "ContaMesas V2"
    This reverts commit ac42805a7b7537d0df4255a6d305668380efe537.

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 #include <math.h>
 
 int contaMesas(int area, int mesasTam, int esp) {
-    int mesas, countLin, countCol, countMesas;
+    int mesas, countLin, countCol;
     countLin = countCol = 0;
-    mesas = 0;
+    mesas = 1;
     area = (int) sqrt(area);
     mesasTam = (int) sqrt(mesasTam);
     int mat[area][area];
@@ -15,13 +15,13 @@ int contaMesas(int area, int mesasTam, int esp) {
             countLin = 0;
         }
         for(int j = 0; j < area; j++) {
-            if (countCol >= esp + mesasTam) {
+            if(countCol >= esp + mesasTam) {
                 countCol = 0;
             }
-            if (countCol < mesasTam && countLin < mesasTam) {
+            if(countCol < mesasTam && countLin < mesasTam) {
                 mat[i][j] = 1;
-                mesas++;
-            } else {
+            }
+            else {
                 mat[i][j] = 0;
             }
             countCol++;
@@ -34,45 +34,6 @@ int contaMesas(int area, int mesasTam, int esp) {
         }
         printf("\n");
     }
-    mesas /= mesasTam * mesasTam;
-    return mesas;
-}
-
-int contaMesasV2(int area, int mesasTam, int esp) {
-    int mesas, countLin, countCol;
-    countLin = countCol = 0;
-    mesas = 0;
-    area = (int) sqrt(area);
-    mesasTam = (int) sqrt(mesasTam);
-    int mat[area][area];
-
-    for(int i = area - 1; i >= 0; i--) {
-        countCol = 0;
-        if(countLin >= esp + mesasTam) {
-            countLin = 0;
-        }
-        for(int j = area - 1; j >= 0; j--) {
-            if (countCol >= esp + mesasTam) {
-                countCol = 0;
-            }
-            if (countCol < mesasTam && countLin < mesasTam
-                && j >= mesasTam && i >= mesasTam) {
-                mat[i][j] = 1;
-                mesas++;
-            } else {
-                mat[i][j] = 0;
-            }
-            countCol++;
-        }
-        countLin++;
-    }
-    for(int i = 0; i < area; i++) {
-        for(int j = 0; j < area; j++) {
-            printf("%d ", mat[i][j]);
-        }
-        printf("\n");
-    }
-    mesas /= mesasTam * mesasTam;
     return mesas;
 }
 
@@ -88,7 +49,7 @@ int main(void) {
     for(i = 0; i < andares; i++) {
         printf("Insira a area(m²) do andar %d a area ocupada pelas mesas e o espacamento entre elas: ", i + 1);
         scanf("%d %d %d", &areaAndar, &mesasTam, &esp);
-        predio[i] = contaMesasV2(areaAndar, mesasTam, esp);
+        predio[i] = contaMesas(areaAndar, mesasTam, esp);
     }
-    printf("%d", predio[0]);
+    printf("%d", predio[1]);
 }
