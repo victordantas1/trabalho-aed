ea9113b89d485de516c8c2213d6878b5ebc803a1
victordantas1 <victorr.dantas@gmail.com>
2024-01-26 13:26:59 -0400

    Adicionando comentarios as funcoes

--- main.c
+++ main.c
@@ -2,10 +2,11 @@
 #include <math.h>
 
 
-void imprimeMatriz(int *mat[], int col) {
+void imprimeMatriz(int *mat[], int col) { // teoricamente eh pra imprimir a matriz
+    int arr[col];
     for(int i = 0; i < col; i++) {
-        int arr[] = mat[i];
         for(int j = 0; j < col; j++) {
+            arr[j] = mat[i][j];
             printf("%d ", arr[j]);
         }
         printf("\n");
@@ -16,36 +17,34 @@ int contaMesas(int area, int mesasTam, int esp) {
     int mesas, countLin, countCol;
     countLin = countCol = 0;
     mesas = 0;
-    area = (int) sqrt(area);
-    mesasTam = (int) sqrt(mesasTam);
+    area = (int) sqrt(area); // passa o tamanho dos lados da sala
+    mesasTam = (int) sqrt(mesasTam); // passa o tamanho dos lados da mesa
     int arr[area];
     int *mat[area];
 
     for(int i = 0 ; i < area; i++) {
         countCol = 0;
-        if(countLin >= esp + mesasTam) {
+        if(countLin >= esp + mesasTam) { // reseta o contador de linhas para que possa imprimir 1 dnv
             countLin = 0;
         }
         for(int j = 0; j < area; j++) {
-            if(countCol >= esp + mesasTam) {
+            if(countCol >= esp + mesasTam) { // reseta o contador de colunas para que possa imprimir 1 dnv
                 countCol = 0;
             }
-            if(countCol < mesasTam && countLin < mesasTam) {
+            if(countCol < mesasTam && countLin < mesasTam) { // insere 1 se os contadores sao menores que o tamanho da mesa
                 arr[j] = 1;
                 mesas++;
             }
-            else {
+            else { // insere 0 se passar do tamanho da mesa
                 arr[j] = 0;
             }
             countCol++;
         }
-        mat[i] = arr;
+        mat[i] = arr; // array de ponteiros recebe arr preenchido com valores
         countLin++;
     }
-
-    imprimeMatriz(mat, area);
     
-    mesas /= mesasTam * mesasTam;
+    mesas /= mesasTam * mesasTam; // insere a quantidade de mesas que a sala comporta
     return mesas;
 }
 
