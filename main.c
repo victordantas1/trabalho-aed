03550c0654777e8a93e0b8155370c6dd71e26529
victordantas1 <victorr.dantas@gmail.com>
2024-01-29 21:31:50 -0400

    29/01

--- main.c
+++ main.c
@@ -2,12 +2,10 @@
 #include <math.h>
 
 
-void imprimeMatriz(int *mat[], int col) { // teoricamente eh pra imprimir a matriz
-    int arr[col];
+void imprimeMatriz(int col, int *mat[]) { // teoricamente eh pra imprimir a matriz
     for(int i = 0; i < col; i++) {
         for(int j = 0; j < col; j++) {
-            arr[j] = mat[i][j];
-            printf("%d ", arr[j]);
+            printf("%d ", mat[i][j]);
         }
         printf("\n");
     }
@@ -19,8 +17,7 @@ int contaMesas(int area, int mesasTam, int esp) {
     mesas = 0;
     area = (int) sqrt(area); // passa o tamanho dos lados da sala
     mesasTam = (int) sqrt(mesasTam); // passa o tamanho dos lados da mesa
-    int arr[area];
-    int *mat[area];
+    int mat[area][area];
 
     for(int i = 0 ; i < area; i++) {
         countCol = 0;
@@ -32,19 +29,18 @@ int contaMesas(int area, int mesasTam, int esp) {
                 countCol = 0;
             }
             if(countCol < mesasTam && countLin < mesasTam) { // insere 1 se os contadores sao menores que o tamanho da mesa
-                arr[j] = 1;
+                mat[i][j] = 1;
                 mesas++;
             }
             else { // insere 0 se passar do tamanho da mesa
-                arr[j] = 0;
+                mat[i][j] = 0;
             }
             countCol++;
         }
-        mat[i] = arr; // array de ponteiros recebe arr preenchido com valores
         countLin++;
     }
 
-    imprimeMatriz(mat, area);
+    imprimeMatriz(area, mat);
     
     mesas /= mesasTam * mesasTam; // insere a quantidade de mesas que a sala comporta
     return mesas;
