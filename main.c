9de8c0688d375795b3b253be89ea6a298f7b0a06
victordantas1 <victorr.dantas@gmail.com>
2024-01-30 11:15:39 -0400

    Adicao de comentarios as funcoes

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include <math.h>
 #include <stdlib.h>
 
-
-void imprimeMatriz(int area, int mat[area][area], int andar) {
+// Funcao para impressao de matriz
+void imprimeMatriz(int area, int mat[area][area], int andar) { 
     printf("\nDisposicao das mesas no andar %d: \n", andar);
     for(int i = 0; i < area; i++) {
         for(int j = 0; j < area; j++) {
@@ -13,17 +13,19 @@ void imprimeMatriz(int area, int mat[area][area], int andar) {
     }
 }
 
+// Funcao para verificar se a area e o tamanho das mesas eh impar
 int verificaImpar(int num1, int num2) {
     if(num1 % 2 != 0 && num2 % 2 != 0) return 1;
     else return 0;
 }
 
-int contaMesas(int area, int sala[area][area], int mesasTam, int espaco) {
+// Funcao para contar a quantidade de mesas que cabem em um andar
+int contaMesas(int area, int andar[area][area], int mesasTam, int espaco) {
     int mesas, countLin, countCol, central;
     countLin = countCol = 0;
     mesas = 0;
   
-    if(area - (espaco + mesasTam) <= 1 && verificaImpar(area, mesasTam)) {
+    if(area - (espaco + mesasTam) <= 1 && verificaImpar(area, mesasTam)) { // Verifica condicao para centralizar quando for somente uma mesa
         mesas = 1;
         central = area / 2;
         for(int i = 0; i < area; i++) {
@@ -32,16 +34,16 @@ int contaMesas(int area, int sala[area][area], int mesasTam, int espaco) {
                 if( i >= central - 1 && j >= central - 1 
                     && (area - countCol) >= mesasTam 
                     && (area - countLin) >= mesasTam) {
-                    sala[i][j] = 1;
+                    andar[i][j] = 1;
                 }
-                else sala[i][j] = 0;
+                else andar[i][j] = 0;
                 countCol++;
             }
             countLin++;
         }
     }
     else {
-        for(int i = 0 ; i < area; i++) {
+        for(int i = 0 ; i < area; i++) { // preenche o andar conforme o tamanho da sala, mesa e espacamento
             countCol = 0;
             if(countLin >= espaco + mesasTam && (area - i) >= mesasTam) { // reseta o contador de linhas para que possa imprimir 1 dnv
                 countLin = 0;
@@ -51,11 +53,11 @@ int contaMesas(int area, int sala[area][area], int mesasTam, int espaco) {
                     countCol = 0;
                 }
                 if(countCol < mesasTam && countLin < mesasTam) { // insere 1 se os contadores sao menores que o tamanho da mesa
-                    sala[i][j] = 1;
+                    andar[i][j] = 1;
                     mesas++;
                 }
                 else { // insere 0 se passar do tamanho da mesa
-                    sala[i][j] = 0;
+                    andar[i][j] = 0;
                 }
                 countCol++;
             }
@@ -74,15 +76,15 @@ int main(void) {
     scanf("%d", &numPredios);
     int predios[numPredios];
 
-    for(j = 0; j < numPredios; j++) {
+    for(j = 0; j < numPredios; j++) { // Laco para iteracao dos predios
         mesasPredio = 0;
         printf("Insira a quantidade de andares do predio %d: ", j + 1);
         scanf("%d", &andares);
         int predio[andares];
-        for(i = 0; i < andares; i++) {
+        for(i = 0; i < andares; i++) { // Laco para iteracao dos andares de cada predio
             printf("\nInsira a area(m²) do andar %d, a area ocupada pelas mesas e o espacamento entre elas: ", i + 1);
             scanf("%d %d %d", &areaAndar, &mesasTam, &esp);
-            areaAndar = (int) sqrt(areaAndar); // passa o tamanho dos lados da sala
+            areaAndar = (int) sqrt(areaAndar); // passa o tamanho dos lados do andar
             mesasTam = (int) sqrt(mesasTam); // passa o tamanho dos lados da mesa
             int andar[areaAndar][areaAndar];
             predio[i] = contaMesas(areaAndar, andar, mesasTam, esp);
